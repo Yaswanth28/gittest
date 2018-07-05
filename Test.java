42c2bcd557e580fb133e689d2d502fc221638df3
Yash <yaswanth.eee222@gmail.com>
2018-07-04 20:32:01 -0500

    Test file uploaded

---
+++ Test.java
@@ -0,0 +1,17 @@
+package com.cts;
+
+public class Test {
+	public static void main(String args[])
+	{
+		String name="khaja,asmath,,hai,,mohammed";
+		String nam[]=name.split(",");
+		for(String na:nam)
+		{
+			System.out.println("value-->"+na);
+		}
+	}
+
+}
+
+
+
